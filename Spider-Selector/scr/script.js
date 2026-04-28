fab2206833f7831773aafd3144f5afb902183b88
Koehler <bernardoskoehler@gmail.com>
2026-04-28 10:36:28 -0300

    Add script.js to handle button click events

---
+++ Spider-Selector/scr/script.js
@@ -0,0 +1,25 @@
+var botao1 = document.getElementById("botao1");
+var botao2 = document.getElementById("botao2");
+var botao3 = document.getElementById("botao3");
+
+var imagem1 = document.getElementById("imagem1");
+var imagem2 = document.getElementById("imagem2");
+var imagem3 = document.getElementById("imagem3");
+
+botao1.onclick = function() {
+    imagem1.src = "https://preview.redd.it/happy-41st-birthday-to-the-amazing-andrew-garfield-v0-dm6ag946mtjd1.jpg";
+    imagem2.src = "https://static0.srcdn.com/wordpress/wp-content/uploads/2022/01/Spider-Man-No-Way-Home-Tobey-Maguire-Emotional.jpg";
+    imagem3.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRIVXiAMGsTc6ZuqqfNaoa5IQFQvGdLzXEoQ&s";
+}
+
+botao2.onclick = function() {
+    imagem1.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3szcvpF9_7DpnF6Zc0rUkKVs-79HBo0FkFw&s";
+    imagem2.src = "https://i.blogs.es/d9b845/tobey-maguire-spidermna-no-way-home/840_560.jpeg";
+    imagem3.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRIVXiAMGsTc6ZuqqfNaoa5IQFQvGdLzXEoQ&s";
+}
+
+botao3.onclick = function() {
+    imagem1.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3szcvpF9_7DpnF6Zc0rUkKVs-79HBo0FkFw&s";
+    imagem2.src = "https://static0.srcdn.com/wordpress/wp-content/uploads/2022/01/Spider-Man-No-Way-Home-Tobey-Maguire-Emotional.jpg";
+    imagem3.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTheXyOOIPK0oGMiMyaBW4ilNwdjNIzGkcNnw&s";
+}
